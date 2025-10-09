fe7890b14f9b271858c6f4a28ab9ca0d779408ab
ArthurSaraiva-Dev <arthursaraiva.dev@gmail.com>
2025-10-09 08:51:09 -0300

    Aula 07

---
+++ Aula07a.py
@@ -0,0 +1,8 @@
+print('---Manipulando Strings---\n')
+nome = input('Qual é seu nome?')
+print('Ola, {:20}! Prazer em te conhecer!'.format(nome))
+print('Ola, {:<20}! Prazer em te conhecer!'.format(nome))
+print('Ola, {:>20}! Prazer em te conhecer!'.format(nome))
+print('Ola, {:^20}! Prazer em te conhecer!'.format(nome))
+print('Ola, {:=^20}! Prazer em te conhecer!'.format(nome))
+
