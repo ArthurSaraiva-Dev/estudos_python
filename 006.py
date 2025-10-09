4c8001993d3a75b25a7de7310ec6248829dcaecf
ArthurSaraiva-Dev <arthursaraiva.dev@gmail.com>
2025-10-09 08:20:51 -0300

    EXERCICIO 006

--- 006.py
+++ 006.py
@@ -1,3 +1,3 @@
 # Crie um algoritmo que leia um número inteiro e mostre na tela o seu dobro, triplo e raiz quadrada.
 num = int(input('Digite um número inteiro:'))
-print('O Dobro de {} é {}, o Triplo é {} e a Raiz Quadrada é {:.2f}'.format(n1))
+print('O Dobro de {} é {}, o Triplo é {} e a Raiz Quadrada é {:.2f}'.format(num, num*2, num*3, num**(1/2)))
