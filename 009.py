09c9a11a82a35738966fb8b074986cd265fd2025
ArthurSaraiva-Dev <arthursaraiva.dev@gmail.com>
2025-10-09 09:15:26 -0300

    Tabuada

---
+++ 009.py
@@ -0,0 +1,7 @@
+# Programa Tabuada
+print('[--- Tabuada ---]')
+
+num = int(input('Digite um núemro para saber sua tabuada: '))
+
+print('A tabuada de {} é \n 1x = {}\n 2x = {}\n 3x = {}\n 4x = {}\n 5x = {}\n 6x = {}\n 7x = {}\n 8x = {}\n 9x = {}\n 10x = {} :'
+      .format(num, num, num*2, num*3, num*4, num*5, num*6, num*7, num*8, num*9, num*10))
