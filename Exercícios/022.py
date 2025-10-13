fcfab28b8324c1b90db8ca8e60718ef5171374c7
ArthurSaraiva-Dev <arthursaraiva.dev@gmail.com>
2025-10-13 09:06:44 -0300

    Nome

---
+++ Exercícios/022.py
@@ -0,0 +1,12 @@
+# Crie um programa que leia o nome completo de uma pessoa e mostre:
+# - O nome com todas as letras maiúsculas e minúsculas.
+# - Quantas letras ao todo (sem considerar espaços).
+# - Quantas letras tem o primeiro nome.
+
+nome = input(print('[--- DIGITE SEU NOME COMPLETO ---]'))
+
+print(nome.upper())
+print(nome.lower())
+print(len(nome) - nome.count(' '))
+print(len(nome.split()[0]))
+
