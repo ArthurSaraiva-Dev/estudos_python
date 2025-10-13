6e5d6321d9ab2ff334f8b786570d0f96c9005f4c
ArthurSaraiva-Dev <arthursaraiva.dev@gmail.com>
2025-10-13 09:13:07 -0300

    Santo na cidade

---
+++ Exercícios/024.py
@@ -0,0 +1,4 @@
+# Exercicio 024 - Verificando o início de um nome de cidade
+# Crie um programa que leia o nome de uma cidade e diga se ela começa ou não com o nome "SANTO".
+cidade = input('Digite o nome de uma cidade: ').strip()
+print(cidade[:5].upper() == 'SANTO')
