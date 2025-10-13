2d964b53df4772088b8797b17671d61f299ef62a
ArthurSaraiva-Dev <arthursaraiva.dev@gmail.com>
2025-10-13 09:21:52 -0300

    Find A

---
+++ Exercícios/026.py
@@ -0,0 +1,10 @@
+# Desafio 026 - Primeira e Última Ocorrência de uma String
+# Faça um programa que leia uma frase pelo teclado e mostre:
+# - Quantas vezes aparece a letra "A".
+# - Em que posição ela aparece a primeira vez.
+# - Em que posição ela aparece a última vez.
+
+frase = input('Digite uma frase: ').strip().upper()
+print('A letra A aparece {} vezes na frase.'.format(frase.count('A')))
+print('A primeira Letra A apareceu na posição {}.'.format(frase.find('A') + 1))
+print('A última Letra A apareceu na posição {}.'.format(frase.rfind('A') + 1))
